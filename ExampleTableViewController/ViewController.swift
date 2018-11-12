a2dd916478cdd75f21372b9ed2cab7b7896ed90e
Kevin Faulhaber <kevin.faulhaber@tuta.io>
2018-11-12 09:49:25 +0100

    Initial Commit

---
+++ ExampleTableViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleTableViewController
+//
+//  Created by Kevin Faulhaber on 12/11/2018.
+//  Copyright © 2018 Julia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
